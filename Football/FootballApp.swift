73e50dfd37171ea36d7df499770b9be69b7751a8
phucnb <kufuc2604@gmail.com>
2025-05-23 13:23:25 -0400

    Initial Commit

---
+++ Football/FootballApp.swift
@@ -0,0 +1,17 @@
+//
+//  FootballApp.swift
+//  Football
+//
+//  Created by Phuc Nguyen on 2025-05-23.
+//
+
+import SwiftUI
+
+@main
+struct FootballApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
